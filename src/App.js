84f12301e9e7ff1a0bd5a5b9562794f7602db1d0
Fahim1837 <fahimrb37@gmail.com>
2024-09-13 11:48:41 +0600

    Router Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import PollList from './components/PollList';
-import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Polls from './custom_modules/polls/Polls';
+import Signup from './custom_modules/signup/Signup';
+import Login from './custom_modules/login/Login';
+import Dashboard from './custom_modules/dashboard/Dashboard';
+import { Route, Router } from 'react-router-dom';
 
 const App = () => (
-  <Switch>
-    <Route exact path='/'>
-      <Polls />
+  <Router >
+    <Route  path='' element= {<Polls/>}/>
+    <Route  path='/signup' element= {<Signup/>}/>
+    <Route  path='/login' element= {<Login/>}/>
+    <Route path= '/protected'>
+      <Route  path='/users' element= {<Users/>}/>
+      <Route  path='/dashboard' element= {<Dashboard/>}/>
+      <Route  path='/pollList' element= {<Dashboard/>}/>
+
     </Route>
-  </Switch>
+
+  </Router>
 );
 
 export default App;
