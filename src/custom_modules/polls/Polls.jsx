6502a9993ed7719b2e5a7e874c455a06452b929b
shamimApploye <shamim.apploye@gmail.com>
2024-09-13 17:59:22 +0600

    polls little modified

--- src/custom_modules/polls/Polls.jsx
+++ src/custom_modules/polls/Polls.jsx
@@ -1,44 +1,44 @@
 import React, { useEffect } from 'react';
-import { PollContainer, Question, ChoiceContainer, ChoiceLabel, Votes, ChoiceInput, NavigationButton } from '../../styledComponents/pollComponents';
+import { Title } from '../../styledComponents/pollComponents';
 import { Container } from '../../styledComponents/common';
 
 
-const PollList = (props) => {
-  // const {pollsList, pollsLoading, pollsError, fetchPolls} = props
+const Polls = (props) => {
+  const {pollsList, pollsLoading, pollsError, fetchPolls} = props
 
-  // useEffect (() => {
-  //   fetchPolls()
-  // },[])
-  // console.log(pollsList)
-  // console.log(typeof fetchPolls)
-  // return (
+  useEffect (() => {
+    fetchPolls()
+  },[])
+  console.log(pollsList)
+
+  return (
     
-  //   <Container>
-  //     <Title>P-Polls: Express Your Opinion</Title>
-  //     <PollContainer>
-  //       <NavigationButton onClick={handlePrev}>&lt;</NavigationButton>
-  //       <div>
-  //         <Question>{poll.question_text}</Question>
-  //         {choices.length > 0 ? (
-  //           choices.map((choice) => (
-  //             <ChoiceContainer key={choice.id}>
-  //               <ChoiceInput
-  //                 type="radio"
-  //                 name={`poll-${poll.id}`}
-  //                 value={choice.id}
-  //               />
-  //               <ChoiceLabel>{choice.choice_text}</ChoiceLabel>
-  //               <Votes>{choice.votes} votes</Votes>
-  //             </ChoiceContainer>
-  //           ))
-  //         ) : (
-  //           <p>No choices available for this poll.</p>
-  //         )}
-  //       </div>
-  //       <NavigationButton onClick={handleNext}>&gt;</NavigationButton>
-  //     </PollContainer>
-  //   </Container>
-  // );
+    <Container>
+      <Title>P-Polls: Express Your Opinion  {pollsList.length}</Title>
+      {/* <PollContainer>
+        <NavigationButton onClick={handlePrev}>&lt;</NavigationButton>
+        <div>
+          <Question>{poll.question_text}</Question>
+          {choices.length > 0 ? (
+            choices.map((choice) => (
+              <ChoiceContainer key={choice.id}>
+                <ChoiceInput
+                  type="radio"
+                  name={`poll-${poll.id}`}
+                  value={choice.id}
+                />
+                <ChoiceLabel>{choice.choice_text}</ChoiceLabel>
+                <Votes>{choice.votes} votes</Votes>
+              </ChoiceContainer>
+            ))
+          ) : (
+            <p>No choices available for this poll.</p>
+          )}
+        </div>
+        <NavigationButton onClick={handleNext}>&gt;</NavigationButton>
+      </PollContainer> */}
+    </Container>
+  );
 };
 
-export default PollList
+export default Polls
