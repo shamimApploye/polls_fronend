c1f28234fb2a57ac184da954ad1349aeabeacf3f
Fahim1837 <fahimrb37@gmail.com>
2024-09-13 12:12:56 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import Polls from './custom_modules/polls/Polls';
 import Signup from './custom_modules/signup/Signup';
 import Login from './custom_modules/login/Login';
 import Dashboard from './custom_modules/dashboard/Dashboard';
-import { Route, Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Users from './custom_modules/users/Users';
 
 function App() {
   return (
-    <Router>
+    <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/polls' element={<Polls />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/protected'>
+      <Route element = {<Dashboard/>}>
         <Route path='/users' element={<Users />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/pollList' element={<Dashboard />} />
-  
+        <Route path='/pollList' element={<Users />} />
       </Route>
-      <Router />
-    </Router>
+    </Routes>
   
   )
 }
